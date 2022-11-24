eef4aa28381191de7a26a61d20c85830c2df1850
fsuknaic <fsuknaic@hotmail.com>
2022-11-24 13:37:14 -0500

    tratamento de erro do aplicativo

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,27 @@
-import express from 'express';
+import 'dotenv/config';
+import { AppError } from '@error/AppError';
+import express, { NextFunction, Request, Response } from 'express';
+
+import { routers } from './routes';
 
 const app = express();
-
 app.use(express.json());
+app.use(routers);
 
-app.get('/', (request, response) => response.send('ola mundo'));
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: err.statusCode,
+      message: err.message,
+    });
+  }
 
-app.listen(3333, () => console.log('server online'));
+  return response.status(500).json({
+    status: 'error',
+    message: 'internal server Error!',
+  });
+});
+
+app.listen(process.env.PORT_SERVER, () =>
+  console.log(`servidor rodando na port ${process.env.PORT_SERVER}`)
+);
