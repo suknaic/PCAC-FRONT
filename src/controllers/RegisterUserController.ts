34ff6b975b921806c3753dbc8326bc02ea55214d
fsuknaic <fsuknaic@hotmail.com>
2022-11-27 14:43:30 -0500

    cadastrar usuario com foto

--- src/controllers/RegisterUserController.ts
+++ src/controllers/RegisterUserController.ts
@@ -4,11 +4,19 @@ import { RegisterUserService } from 'services/RegisterUserService';
 class RegisterUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { nome, cpf, email, telefone, senha } = request.body;
+    const avatar = request.file.filename;
 
     const registerUserService = new RegisterUserService();
 
     try {
-      registerUserService.execute({ nome, cpf, email, telefone, senha });
+      registerUserService.execute({
+        image: avatar,
+        nome,
+        cpf,
+        email,
+        telefone,
+        senha,
+      });
       return response.status(200).send();
     } catch (error) {
       return response.json({ error: error.message });
--- src/routes/registerRouter.ts
+++ src/routes/registerRouter.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
+import Multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import { RegisterUserController } from '../controllers/RegisterUserController';
 
 const registerRouter = Router();
-
 const registerUserController = new RegisterUserController();
 
-registerRouter.post('/cadastro', registerUserController.handle);
+const avatarUpload = Multer(uploadConfig.upload('avatar'));
+
+registerRouter.post(
+  '/cadastro',
+  avatarUpload.single('avatar'),
+  registerUserController.handle
+);
 
 export { registerRouter };
