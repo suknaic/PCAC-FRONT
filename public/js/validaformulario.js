848fd62ae5ab22c449cb7f442309ebec83a35e4a
Felipe Suknaic <fsuknaic@hotmail.com>
2022-12-06 21:07:55 -0500

    pagina inicial finalizada

--- public/js/validaformulario.js
+++ public/js/validaformulario.js
@@ -17,9 +17,4 @@
       false
     );
   });
-
-  $('#cep').mask('00000-000');
-  $('#cpf').mask('000.000.000-00', { reverse: false });
-  $('#cnpj').mask('00.000.000/0000-00', { reverse: false });
-  $('#telefone').mask('(00) 00000-0000');
 })();
